961d0dbccfcb618758afd7c4c3f2dba5b38125c0
DewansyahPixel <dewansyahfirdaus@gmail.com>
2024-11-20 14:14:30 +0700

    Projek Tahapn 4

---
+++ app/src/main/java/com/example/level1/Widget.kt
@@ -0,0 +1,74 @@
+package com.example.level1
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
+@Composable
+@Preview
+
+fun Widget(){
+    Row(
+        modifier = Modifier
+            .width(162.dp)
+            .height(188.dp)
+            .background(Color.White)
+            .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+    ){
+        Box(
+            modifier = Modifier
+                .padding()
+        )
+        Card(modifier = Modifier
+            .width(162.dp)
+            .height(188.dp))
+        {
+            Card(modifier = Modifier
+                .height(90.dp)
+                .width(130.dp),
+                colors = CardDefaults.cardColors(Color.Red)
+            ) { }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Nama Product", fontSize = 12.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(2.dp))
+                Card (
+                    modifier = Modifier
+                        .width(40.dp)
+                        .height(13.dp),
+                    colors = CardDefaults.cardColors(Color.Blue)
+                ){  Box (modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                )
+                {Text("Rating", fontSize = 8.sp, fontWeight = FontWeight.Normal)  } }
+            }
+
+
+        }
+    }
+}
