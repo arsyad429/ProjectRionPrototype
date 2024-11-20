6c36f59daab5189c16606aab86cc801d0e5042c7
trishulaa <sembulungan3@gmail.com>
2024-11-20 14:19:12 +0700

    project tahap 3

--- app/src/main/java/com/example/level1/productScreen.kt
+++ app/src/main/java/com/example/level1/productScreen.kt
@@ -1,22 +1,35 @@
 package com.example.level1
 
+import android.provider.CalendarContract.Colors
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
 
-fun productScren(){
-LazyColumn (
-    modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)
+fun productScren() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White)
     ) {
-
+    items(4){
+    }
     }
 }
--- app/src/main/java/com/example/level1/productShoesEntity.kt
+++ app/src/main/java/com/example/level1/productShoesEntity.kt
@@ -6,3 +6,61 @@ data class productShoesEntity(
     val imageURL : String
 
 )
+
+object productShoesObject {
+    val sepatusuper = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper1 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper2 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper3 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper4 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper5 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper6 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper7 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper8 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper9 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+    val sepatusuper10 = productShoesEntity(
+        name = "Sepatu Super",
+        rating = 4.5,
+        imageURL = ""
+    )
+}
